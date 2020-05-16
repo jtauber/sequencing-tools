080d343cbe4002a1d8b7c79972abff9dd581d483
James Tauber <jtauber@jtauber.com>
2020-05-16 15:29:15 +0800

    slightly refactored example

--- example1.py
+++ example1.py
@@ -11,21 +11,25 @@ from sequencing_tools.ordering import frequency, frequency_optimised, next_best
 item_type = TokenType.lemma
 target_type = ChunkType.verse
 
-target_items = get_tokens_by_chunk(TokenType.lemma, ChunkType.verse)
+target_items = get_tokens_by_chunk(item_type, target_type)
+
+
+def get_text(chunk_type, chunk_id):
+    return " ".join(get_tokens(TokenType.text, target_type, target))
 
 
 for target, items_to_learn in islice(frequency(target_items), 10):
-    print(target, " ".join(get_tokens(TokenType.text, ChunkType.verse, target)))
+    print(target, get_text(target_type, target))
     print(items_to_learn)
 
 print()
 
 for target, items_to_learn in islice(frequency_optimised(target_items), 10):
-    print(target, " ".join(get_tokens(TokenType.text, ChunkType.verse, target)))
+    print(target, get_text(target_type, target))
     print(items_to_learn)
 
 print()
 
 for target, items_to_learn in islice(next_best(target_items), 10):
-    print(target, " ".join(get_tokens(TokenType.text, ChunkType.verse, target)))
+    print(target, get_text(target_type, target))
     print(items_to_learn)
